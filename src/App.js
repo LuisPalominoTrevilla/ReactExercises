c48819fd43a322d920a108d985f157806f484ce5
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-06-10 10:51:54 -0500

    Configured app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-light" style={{background: '#e3f2fd'}}>
+          <h2>Hello World</h2>
+        </nav>
       </div>
     );
   }
