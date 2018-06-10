56703a6ff2ebd8744fa65db2239d8ac3f2123712
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-06-10 11:37:25 -0500

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+//import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <nav className="navbar navbar-light" style={{background: '#e3f2fd'}}>
           <h2>Hello World</h2>
         </nav>
+        <Menu/>
       </div>
     );
   }
