c63b4d88e2f8136f10b62a475fe9ae5dbaf9fb03
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-06-10 14:44:19 -0500

    Components part 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React, { Component } from 'react';
 //import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/menuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-light" style={{background: '#e3f2fd'}}>
           <h2>Hello World</h2>
         </nav>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
