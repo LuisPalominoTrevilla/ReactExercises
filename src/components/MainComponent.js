49a4cc8b839a6945413848da6eb3e6a13f73912e
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-06-13 10:51:17 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './menuComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
 import Header from './headerComponent';
 import Footer from './footerComponent';
@@ -13,7 +17,10 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -21,7 +28,7 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} leader={this.state.leaders.filter((leader) => leader.featured)[0]} promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}/> // Return only feature dish using filter js
       );
     }
 
@@ -31,6 +38,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -23,7 +23,7 @@ class Header extends Component {
         return(
             <React.Fragment>
                 <Navbar dark expand="md">
-                    <div className="Container">
+                    <div className="container">
                         <NavbarToggler onClick={this.toggleNav} /> 
                         <NavbarBrand className="mr-auto" href="/">
                             <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion"/>
