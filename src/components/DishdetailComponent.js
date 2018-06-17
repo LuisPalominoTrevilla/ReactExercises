6cb874466c719cabdf851f117c59ae30b3364e58
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-06-16 22:08:16 -0500

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -23,7 +23,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         console.log(JSON.stringify(values));
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     toogleModal() {
@@ -109,7 +109,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}){
+function RenderComments({comments, postComment, dishId}){
     const comm = comments.map((comment) => {
         return(
             <div key={comment.id}>
@@ -125,7 +125,7 @@ function RenderComments({comments, addComment, dishId}){
             <ul className="list-unstyled">
             {comm}
             </ul>
-            <CommentForm dishId ={dishId} addComment={addComment}/>
+            <CommentForm dishId ={dishId} postComment={postComment}/>
         </div>
     );
 }
@@ -164,7 +164,7 @@ const DishDetail = (props) => {
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.selectedDish.id}/>
                     </div>
                 </div>
